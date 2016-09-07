70784c7d5fad8778746a49088aebb116f023c369
af <af@POSTE315.luisina.fr>
2016-09-07 14:33:02 +0200

    Merge portail_fournisseur/master

--- connexDB2.php
+++ connexDB2.php
@@ -6,7 +6,7 @@ $user = $_SESSION['CodeUser'];
  
 	// CONVENTION APPELATION SYSTEME : DB2_I5_NAMING_ON  (appelation avec / au lieu  de .) et 
 	// utilisation des jobd 
-$option = array('i5_libl'=>'EURO4SCDG SPECSCD EURO4SCD   EURO4PRG TEUR4SCD QTEMP' , 'i5_naming'=>DB2_I5_NAMING_ON,'i5_commit'=>DB2_I5_TXN_NO_COMMIT); 
+$option = array('i5_libl'=>'EURO4SCDG SPECSCD EURO4SCD   EURO4PRG TEUR4SCD QTEMP XSCDXPRG' , 'i5_naming'=>DB2_I5_NAMING_ON,'i5_commit'=>DB2_I5_TXN_NO_COMMIT); 
 $DB = db2_connect("IFS", $user , $pwd , $option);
 if (!$DB) {
 echo "<b>Connect Failed: " . db2_conn_errormsg() . "</b>";

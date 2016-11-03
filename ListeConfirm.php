2639c56f72b3a59697c99ebb401d56cdb50531f5
af <af@POSTE315.luisina.fr>
2016-11-03 17:19:50 +0100

    Merge portail_fournisseur/master

--- ListeConfirm.php
+++ ListeConfirm.php
@@ -20,12 +20,12 @@ $P3NFOU  = $_SESSION['P3NFOU'];
 
 echo "<TABLE   border='2'> <caption>liste des Commandes de :    " . $P3NFOU ;
 echo "</caption>";
-echo "<tr><th>commande</th><th>vos refs</th><th>article</th><th>libelle art</th><th>nos refs </th><th>Date</th><th>selection</th></tr>";
+echo "<tr><th>commande</th><th>vos refs</th><th>article</th><th>libelle art</th><th>nos refs </th><th>Date</th><th>confirmation</th><th>selection</th>  </tr>";
 
 // la connexion a la BDD
 include ("connexDB2.php");
 
-$requet1 = "   with temp as ( SELECT p.adoc, p.alig , p.nart , P.lcdl , p.DCAR ,    p.ORFD , p.OLIG , c.vref , c.nref ,
+$requet1 = "   with temp as ( SELECT p.adoc, p.alig , p.nart , P.lcdl , p.DCAR ,    p.ORFD , p.OLIG , c.vref , c.nref , p.fcar ,
 			row_number() over( order by p.DCAR , c.vref , p.adoc) as cdefou from   euro4scd/cdflig6   P  inner  join cdfent c
 		on p.adoc = c.adoc
             where  P.tygs = 'GCO' and  c.vref <> '  ' and c.atdo = 'CF' 
@@ -53,7 +53,7 @@ if (empty ( $sens )) {
 
 $requet2 =   $P3NFOU . "' ";
 
-$requet3 = "  ) select   t.adoc, t.vref , t.nref  , t.nart , t.lcdl , t.DCAR ,   t.ORFD , t.OLIG , t.alig   from temp   t 
+$requet3 = "  ) select   t.adoc, t.vref , t.nref  , t.nart , t.lcdl , t.DCAR ,   t.ORFD , t.OLIG , t.alig ,t.fcar  from temp   t 
 		where t.cdefou between " . $posdeb . "  and " . $posfin  . "  order by t.cdefou "    ;
 
 $requet = $requet1 . $requet2 . $requet3;
@@ -85,6 +85,10 @@ if ($result == 0) {
 		echo "</td><td>";
 		echo $lignes ['DCAR'];
 		echo "</td><td>";
+                    if ($lignes ['FCAR'] == 0) {
+                    echo 'Non';}
+                    else{ echo 'Oui';}
+                echo "</td><td>";
 		// je rajoute un formulaire pour d�tail BL
 		echo '<form method = "POST" action="confirm.php" target="_self">';
 		echo '<input type="hidden" name="ADOC"  value ="' . $lignes ['ADOC'] . '">';

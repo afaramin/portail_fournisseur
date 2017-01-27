eb5fb54f72ca571a3a37b3e0d9c6e84057028109
af <af@POSTE315.luisina.fr>
2017-01-27 18:00:34 +0100

    Merge portail_fournisseur/master

--- testxwebinfprd.php
+++ testxwebinfprd.php
@@ -2,30 +2,68 @@
     
 <head>
     <meta charset="utf-8">
-    <title>test webservice</title>
+    <title>test client  webservice</title>
      <link rel="stylesheet" type="text/css"  href="./css/monstyle.css">
 </head>
 <body>
+    
+    <script language="JavaScript">
+   
+    
+    function ferme_fenetre() {
+        top.close();
+        }
 
     
+  </script>
+   
+
+    <a href="javascript: void ferme_fenetre()">Fermer</a>
     
 <?php
 
-echo
+ if(isset($_POST['btn_sub']))
+{
  
-$start = microtime(true);
-$client = new SoapClient('http://95.128.145.120:10010/web/services/XWEBINFPRD/xwebinfprd.wsdl', array('cache_wsdl' => WSDL_CACHE_NONE));
-    $response = $client->xwebinfprd(
-        array(
-          'args0' => array(
-            'PART'=> 'EV6221 273',
-          ),
-        )
-    );
-    
-print_r($response);
-echo microtime(true) - $start;
+//$start = microtime(true);
+//$client = new SoapClient('http://95.128.145.120:10010/web/services/XWEBINFPRD/xwebinfprd.wsdl', array('cache_wsdl' => WSDL_CACHE_NONE));
+$critere = $_POST["nom"]; 
+echo $critere;
+
+$client = new SoapClient('http://95.128.145.120:10010/web/services/XWEBINFPRD/xwebinfprd.wsdl');
+
+
+$err= $client->getError();
+echo $err;
+if($err) { die('erreur de connexion au service: '.$err);
+}
+else {
+    $param= array("args0" => array("PART" => $critere));
+    $response = $client->xwebinfprd($param);
+    $err= $client->getError();
+    if($err) die('erreur d\'execution du service: '.$err);
+        
+     echo $response ;
+             
+    // echo microtime(true) - $start;
+    echo 'test  fin ';
+    }
+}
+
+else {
+ echo'<form method="post" action="testxwebinfprd.php">
+<label for="article">Nom: </label>
+<input type="text" name="nom"><br>';
+echo $response ;
+echo'<input type="submit" name="btn_sub" value="Envoyer">
+</form>'; 
+
+ }
+
 
 ?>
+
+
+
 </body>
 </html>
